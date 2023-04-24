622ae71e06fd170f2867f8f5e6a4e3bec292669e
cypher33 <rohitkumar25.rkl@gmail.com>
2023-04-24 22:15:12 +0530

    added login and logout feats

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
-import Cookie from 'universal-cookie';
+import Cookies from 'universal-cookie';
 
-import { ChannelListContainer, ChannelContainer } from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
 import './App.css';
 
 const STREAM_API_KEY = 'rcqrxpzueju5';
 
+const cookies = new Cookies();
+const AUTH_TOKEN = cookies.get("token");
+
 const streamClient = StreamChat.getInstance(STREAM_API_KEY);
 
+if(AUTH_TOKEN) {
+    streamClient.connectUser({
+        name: cookies.get("username"),
+        fullName: cookies.get("fullName"),
+        id: cookies.get("userId"),
+        phoneNumber: cookies.get("phoneNumber"),
+        image: cookies.get("avatarURL"),
+        hashedPassword: cookies.get("hashedPassword")
+    }, AUTH_TOKEN);
+}
+
 const App = () => {
+    if(!AUTH_TOKEN) return <Auth />
+
     return (
         <div className="app__wrapper">
             <Chat client={streamClient} theme="team light">
